56afb78fc35aa992c155bd4ffca4e891b460c6c8
omoniwa olabode chris <omoniwachris@gmail.com>
2022-10-17 07:06:21 -0700

    added a main.h file

---
+++ main.h
@@ -0,0 +1,27 @@
+#ifndef PRINT_F
+#define PRINT_F
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/* main function */
+
+
+
+
+int _write_char(char);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
+int print_integer(va_list);
+int print_number(va_list);
+int print_binary(va_list);
+int print_reversed(va_list arg);
+int rot13(va_list);
+int unsigned_integer(va_list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_heX(va_list list);
+
+#endif
