374a1e1cb0e81a2b2107eed7850a16b84f12541d
omoniwa olabode chris <omoniwachris@gmail.com>
2022-10-18 08:35:06 -0700

    main.h file

--- main.h
+++ main.h
@@ -4,24 +4,46 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <limits.h>
 
-/* main function */
+/**
+ * struct special_cases -struct for special cases
+ *
+ * @match: the special character t match after percent sign
+ * @fnct: The function associated
+ *
+ *
+ */
+typedef struct special_cases;
+{
+	char *match;
+	int (*fnct)(va_list);
+}spec_f;
 
-
+ * /* main function */
 
 int _printf(const char *format, ...);
-int _write_char(char);
-int print_char(va_list);
-int print_string(va_list);
-int print_percent(va_list);
-int print_integer(va_list);
-int print_number(va_list);
-int print_binary(va_list);
-int print_reversed(va_list arg);
-int rot13(va_list);
-int unsigned_integer(va_list);
-int print_octal(va_list list);
-int print_hex(va_list list);
-int print_heX(va_list list);
+/*format and print data*/
 
-#endif
+int _write_char(char c);
+/*writes the character c to stdout*/
+
+int (*mod_character_s(const char *next, int mtch))(va_list);
+/*search for match and execute the function according to this*/
+
+int print_char(va_list c);
+/*prints a char*/
+
+int print_string(va_list s);
+/*prints a string */
+
+int print_number(va_list i);
+/*converts a string to an integer and prints its content.*/
+
+int print_unsigned(va_list u);
+/*converts a string to an unsigned integer and prints its content*/
+
+int print_reverse(va_list r);
+/*prints a string in reverse*/
+
+#endif /* PRINT_F */
