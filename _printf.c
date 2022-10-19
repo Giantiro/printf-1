78e43b59a5958bb1285975918e23f169eb44b684
Anthonia <giantonia001@gmail.com>
2022-10-19 04:13:48 -0700

    Print number

--- _printf.c
+++ _printf.c
@@ -3,8 +3,8 @@
 /**
  * _printf - produces output according to a format
  * @format: format string containing the characters and the specifiers
- * Description: this function will call the get_print() function that will
- * determine which printing function to call depending on the conversion
+ * Description: Function calls the get_print() function that
+ * determines which printing function to call based on the conversion
  * specifiers contained into fmt
  * Return: length of the formatted output string
  */
